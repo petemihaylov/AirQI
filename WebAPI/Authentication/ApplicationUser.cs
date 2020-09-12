41a0b13b802de2f793c8bbd4d52ed234b25ad2ac
Mihaylov,Petar P <p.mihaylov@student.fontys.nl>
2020-09-12 19:23:50 +0200

    jwt web api init

---
+++ WebAPI/Authentication/ApplicationUser.cs
@@ -0,0 +1,9 @@
+using Microsoft.AspNetCore.Identity;
+
+namespace WebAPI.Authentication  
+***REMOVED***  
+    public class ApplicationUser: IdentityUser  
+    ***REMOVED***  
+
+   ***REMOVED***  
+***REMOVED***
---
+++ WebAPI/Authentication/UserRoles.cs
@@ -0,0 +1,8 @@
+namespace WebAPI.Authentication  
+***REMOVED***  
+    public static class UserRoles  
+    ***REMOVED***  
+        public const string Admin = "Admin";  
+        public const string User = "User";  
+   ***REMOVED***  
+***REMOVED*** 

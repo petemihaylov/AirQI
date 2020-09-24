80e8a966d6c99cf707d30cf94d40fee4a91e85b2
Mihaylov,Petar P <p.mihaylov@student.fontys.nl>
2020-09-24 15:07:48 +0200

    removing old files

--- WebAPI/Authentication/UserRoles.cs
+++
@@ -1,8 +0,0 @@
-namespace WebAPI.Authentication  
-***REMOVED***  
-    public static class UserRoles  
-    ***REMOVED***  
-        public const string Admin = "Admin";  
-        public const string User = "User";  
-   ***REMOVED***  
-***REMOVED*** 

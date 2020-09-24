3b4e4fc764da2b37ebc050d9cdebfff1e62607d6
Mihaylov,Petar P <p.mihaylov@student.fontys.nl>
2020-09-24 15:07:48 +0200

    removing old files

--- WebAPI/Authentication/ApplicationUser.cs
+++
@@ -1,9 +0,0 @@
-using Microsoft.AspNetCore.Identity;
-
-namespace WebAPI.Authentication  
-***REMOVED***  
-    public class ApplicationUser: IdentityUser  
-    ***REMOVED***  
-
-   ***REMOVED***  
-***REMOVED***
--- WebAPI/Authentication/Response.cs
+++
@@ -1,9 +0,0 @@
-namespace WebAPI.Authentication
-***REMOVED***
-    public class Response
-    ***REMOVED***
-        public string Status ***REMOVED*** get; set;***REMOVED***
-        public string Message ***REMOVED*** get; set;***REMOVED***
-
-   ***REMOVED***
-***REMOVED***

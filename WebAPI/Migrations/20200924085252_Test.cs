3c9897342d253710ed69d550ef5bcfade43b46b1
Mihaylov,Petar P <p.mihaylov@student.fontys.nl>
2020-09-24 12:17:50 +0200

    Dockerfile and .dockerignore

---
+++ WebAPI/Migrations/20200924085252_Test.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace WebAPI.Migrations
+***REMOVED***
+    public partial class Test : Migration
+    ***REMOVED***
+        protected override void Up(MigrationBuilder migrationBuilder)
+        ***REMOVED***
+
+       ***REMOVED***
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        ***REMOVED***
+
+       ***REMOVED***
+   ***REMOVED***
+***REMOVED***

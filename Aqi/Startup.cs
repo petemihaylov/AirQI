3d924efedc8d46ed703a93b8c22683b87e341862
Konstantin Mihaylov <mihailovkonstantin364@gmail.com>
2020-09-25 17:44:18 +0200

    Set-up

---
+++ Aqi/Startup.cs
@@ -0,0 +1,51 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+
+namespace Aqi
+***REMOVED***
+    public class Startup
+    ***REMOVED***
+        public Startup(IConfiguration configuration)
+        ***REMOVED***
+            Configuration = configuration;
+       ***REMOVED***
+
+        public IConfiguration Configuration ***REMOVED*** get;***REMOVED***
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        ***REMOVED***
+            services.AddControllers();
+       ***REMOVED***
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        ***REMOVED***
+            if (env.IsDevelopment())
+            ***REMOVED***
+                app.UseDeveloperExceptionPage();
+           ***REMOVED***
+
+            app.UseHttpsRedirection();
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            ***REMOVED***
+                endpoints.MapControllers();
+           ***REMOVED***);
+       ***REMOVED***
+   ***REMOVED***
+***REMOVED***

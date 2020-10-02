6250d5a3fa45ab7ebc4a33a4dd9793b933721ace
Mihaylov,Petar P <p.mihaylov@student.fontys.nl>
2020-10-02 11:35:58 +0200

    Adds Docker files to Jwt and Base, adds Bearer Authentication to jwt

---
+++ ApiJwt/Models/User.cs
@@ -0,0 +1,16 @@
+using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace ApiJwt.Models
+***REMOVED***
+    public class User : BaseEntity
+    ***REMOVED***
+        
+        [MaxLength(250)]
+        public string LastName ***REMOVED*** get; set;***REMOVED***
+
+        [Required]
+        public string Password ***REMOVED*** get; set;***REMOVED***    
+   ***REMOVED***
+    
+***REMOVED***
--- ApiJwt/Program.cs
+++ ApiJwt/Program.cs
@@ -7,7 +7,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 
-namespace ApiBase
+namespace ApiJwt
 ***REMOVED***
     public class Program
     ***REMOVED***
@@ -21,7 +21,6 @@ namespace ApiBase
                 .ConfigureWebHostDefaults(webBuilder =>
                 ***REMOVED***
                     webBuilder.UseStartup<Startup>();
-                    // webBuilder.UseUrls("http://localhost:8080/");
                ***REMOVED***);
    ***REMOVED***
 ***REMOVED***

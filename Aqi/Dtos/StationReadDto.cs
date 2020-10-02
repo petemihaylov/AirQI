156dea69f513686f2ffa5d4aecf3fda5ba3d5f5c
Konstantin Mihaylov <mihailovkonstantin364@gmail.com>
2020-10-02 20:39:14 +0200

    Update Station Dtos

--- Aqi/Dtos/StationReadDto.cs
+++ Aqi/Dtos/StationReadDto.cs
@@ -5,26 +5,18 @@ using System.ComponentModel.DataAnnotations;
 
 namespace Aqi.Dtos
 ***REMOVED***
-    public class StationCreateDto
+    public class StationReadDto
     ***REMOVED***
-        [Required]
         public City City ***REMOVED*** get; set;***REMOVED***
 
-        [Required]
         public Country Country ***REMOVED*** get; set;***REMOVED***
 
-        [Required]
         public Location Location ***REMOVED*** get; set;***REMOVED***
-
-        [Required]
+        
         public ICollection<Measurement> Measurements ***REMOVED*** get; set;***REMOVED***
 
-        [Required]
-        [DataType(DataType.Date)]
         public DateTime CreatedAt ***REMOVED*** get; set;***REMOVED***
 
-        [Required]
-        [DataType(DataType.Date)]
         public DateTime UpdatedAt ***REMOVED*** get; set;***REMOVED***
    ***REMOVED***
 ***REMOVED***

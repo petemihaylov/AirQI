5115bbf618988a610968cd9c6b4e051cb2584f48
Mihaylov,Petar P <p.mihaylov@student.fontys.nl>
2020-10-08 12:04:51 +0200

    services for auth

--- client/src/components/App/index.js
+++ client/src/components/App/index.js
@@ -2,6 +2,7 @@ import React, ***REMOVED*** Component***REMOVED*** from "react";
 import ***REMOVED*** BrowserRouter as Router, Switch, Route***REMOVED*** from "react-router-dom";
 import Dashboard from "../Dashboard";
 import RegisterForm from "../RegisterForm";
+import Login from "../login.component";
 
 class App extends Component ***REMOVED***
   render() ***REMOVED***
@@ -12,6 +13,7 @@ class App extends Component ***REMOVED***
             <Dashboard />
           </Route>
           <Route path="/register" component=***REMOVED***RegisterForm***REMOVED*** />
+          <Route path="/signin" component=***REMOVED***Login***REMOVED*** />
         </Switch>
       </Router>
     );
---
+++ client/src/components/login.component.js
@@ -0,0 +1,155 @@
+import React, ***REMOVED*** Component***REMOVED*** from "react";
+import Form from "react-validation/build/form";
+import Input from "react-validation/build/input";
+import CheckButton from "react-validation/build/button";
+import AuthService from "../services/auth.service";
+import Container from "@material-ui/core/Container";
+import Avatar from "@material-ui/core/Avatar";
+
+const required = (value) => ***REMOVED***
+  if (!value) ***REMOVED***
+    return (
+      <div className="alert alert-danger" role="alert">
+        This field is required!
+      </div>
+    );
+ ***REMOVED***
+***REMOVED***;
+
+export default class Login extends Component ***REMOVED***
+  constructor(props) ***REMOVED***
+    super(props);
+    this.handleLogin = this.handleLogin.bind(this);
+    this.onChangeUsername = this.onChangeUsername.bind(this);
+    this.onChangePassword = this.onChangePassword.bind(this);
+
+    this.state = ***REMOVED***
+      username: "",
+      password: "",
+      loading: false,
+      message: "",
+   ***REMOVED***;
+ ***REMOVED***
+
+  onChangeUsername(e) ***REMOVED***
+    this.setState(***REMOVED***
+      username: e.target.value,
+   ***REMOVED***);
+ ***REMOVED***
+
+  onChangePassword(e) ***REMOVED***
+    this.setState(***REMOVED***
+      password: e.target.value,
+   ***REMOVED***);
+ ***REMOVED***
+
+  handleLogin(e) ***REMOVED***
+    e.preventDefault();
+
+    this.setState(***REMOVED***
+      message: "",
+      loading: true,
+   ***REMOVED***);
+
+    this.form.validateAll();
+
+    if (this.checkBtn.context._errors.length === 0) ***REMOVED***
+      AuthService.login(this.state.username, this.state.password).then(
+        () => ***REMOVED***
+          this.props.history.push("/profile");
+          window.location.reload();
+       ***REMOVED***,
+        (error) => ***REMOVED***
+          const resMessage =
+            (error.response &&
+              error.response.data &&
+              error.response.data.message) ||
+            error.message ||
+            error.toString();
+
+          this.setState(***REMOVED***
+            loading: false,
+            message: resMessage,
+         ***REMOVED***);
+       ***REMOVED***
+      );
+   ***REMOVED*** else ***REMOVED***
+      this.setState(***REMOVED***
+        loading: false,
+     ***REMOVED***);
+   ***REMOVED***
+ ***REMOVED***
+
+  render() ***REMOVED***
+    return (
+      <Container maxWidth="md">
+        <div className="col-md-12">
+          <div className="card card-container p-5 mt-4">
+            <Avatar
+              alt="Remy Sharp"
+              src="//ssl.gstatic.com/accounts/ui/avatar_2x.png"
+              className="mb-4 mt-3"
+            />
+
+            <Form
+              onSubmit=***REMOVED***this.handleLogin***REMOVED***
+              ref=***REMOVED***(c) => ***REMOVED***
+                this.form = c;
+             ***REMOVED******REMOVED***
+            >
+              <div className="form-group">
+                <label htmlFor="username">Username</label>
+                <Input
+                  type="text"
+                  className="form-control"
+                  name="username"
+                  value=***REMOVED***this.state.username***REMOVED***
+                  onChange=***REMOVED***this.onChangeUsername***REMOVED***
+                  validations=***REMOVED***[required]***REMOVED***
+                />
+              </div>
+
+              <div className="form-group">
+                <label htmlFor="password">Password</label>
+                <Input
+                  type="password"
+                  className="form-control"
+                  name="password"
+                  value=***REMOVED***this.state.password***REMOVED***
+                  onChange=***REMOVED***this.onChangePassword***REMOVED***
+                  validations=***REMOVED***[required]***REMOVED***
+                />
+              </div>
+
+              <div className="form-group">
+                <button
+                  className="btn btn-primary btn-block"
+                  disabled=***REMOVED***this.state.loading***REMOVED***
+                >
+                  ***REMOVED***this.state.loading && (
+                    <span className="spinner-border spinner-border-sm"></span>
+                  )***REMOVED***
+                  <span>Login</span>
+                </button>
+              </div>
+
+              ***REMOVED***this.state.message && (
+                <div className="form-group">
+                  <div className="alert alert-danger" role="alert">
+                    ***REMOVED***this.state.message***REMOVED***
+                  </div>
+                </div>
+              )***REMOVED***
+              <CheckButton
+                style=***REMOVED******REMOVED*** display: "none"***REMOVED******REMOVED***
+                ref=***REMOVED***(c) => ***REMOVED***
+                  this.checkBtn = c;
+               ***REMOVED******REMOVED***
+              />
+            </Form>
+          </div>
+        </div>
+      </Container>
+    );
+ ***REMOVED***
+***REMOVED***

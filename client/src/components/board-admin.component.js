f49c76b0f388370305b54520df5c9a640e6f7d80
Mihaylov,Petar P <p.mihaylov@student.fontys.nl>
2020-10-09 13:36:35 +0200

    Adds Services and Home, Admin, Moderator components

---
+++ client/src/components/board-admin.component.js
@@ -0,0 +1,40 @@
+import React, ***REMOVED*** Component***REMOVED*** from "react";
+import UserService from "../services/user.service";
+
+export default class BoardAdmin extends Component ***REMOVED***
+    constructor(props) ***REMOVED***
+        super(props);
+
+        this.state = ***REMOVED***
+            content: ""
+       ***REMOVED***;
+   ***REMOVED***
+
+    componentDidMount() ***REMOVED***
+        UserService.getPublicContent().then(
+            response => ***REMOVED***
+                this.setState(***REMOVED***
+                    content: response.data
+               ***REMOVED***);
+           ***REMOVED***,
+            error => ***REMOVED***
+                this.setState(***REMOVED***
+                    content:
+                        (error.response && error.response.data) ||
+                        error.message ||
+                        error.toString()
+               ***REMOVED***);
+           ***REMOVED***
+        );
+   ***REMOVED***
+
+    render() ***REMOVED***
+        return (
+            <div className="container">
+                <header className="jumbotron">
+                    <h3>***REMOVED***this.state.content***REMOVED***</h3>
+                </header>
+            </div>
+        );
+   ***REMOVED***
+***REMOVED***

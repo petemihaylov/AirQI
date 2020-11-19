cf1507bfbcca3c462bc4150caafe62378b01a5d7
Konstantin Mihaylov <mihailovkonstantin364@gmail.com>
2020-11-19 12:42:58 +0100

    SignalR Configurations

---
+++ Aqi/Hubs/StationHub.cs
@@ -0,0 +1,15 @@
+﻿using System.Threading.Tasks;
+using Aqi.Hubs.Clients;
+using Aqi.Models.Data;
+using Microsoft.AspNetCore.SignalR;
+
+namespace AssetNXT.Hubs
+***REMOVED***
+    public class RuuviStationHub : Hub<IStationClient>
+    ***REMOVED***
+        public async Task SendRuuviStation(Station station)
+        ***REMOVED***
+            await Clients.All.ReceiveStation(station);
+       ***REMOVED***
+   ***REMOVED***
+***REMOVED***

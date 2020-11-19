c4e8519daa39c60498517af8af63e01fd990eeeb
Konstantin Mihaylov <mihailovkonstantin364@gmail.com>
2020-11-19 12:10:48 +0100

    Update MeasurementCreateDto.cs

--- Aqi/Dtos/MeasurementCreateDto.cs
+++ Aqi/Dtos/MeasurementCreateDto.cs
@@ -1,11 +1,42 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using Aqi.Models.Data;
+using System.ComponentModel.DataAnnotations;
 
 namespace Aqi.Dtos
 {
     public class MeasurementCreateDto
     {
+        [Required]
+        public Location Location { get; set; }
+
+        [Required]
+        public double Pm025 { get; set; }
+
+        [Required]
+        public double Pm100 { get; set; }
+
+        [Required]
+        public double Aqi { get; set; }
+
+        [Required]
+        public double P { get; set; }
+
+        [Required]
+        public double H { get; set; }
+
+        [Required]
+        public double T { get; set; }
+
+        [Required]
+        public double O3 { get; set; }
+
+        [Required]
+        public double No2 { get; set; }
+
+        [Required]
+        public DateTime CreatedAt { get; set; }
+
+        [Required]
+        public DateTime UpdatedAt { get; set; }
     }
 }

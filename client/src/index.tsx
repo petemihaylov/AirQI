48d5e0eea997657ee5f9188e2dc7022e112b4440
Konstantin Mihaylov <mihailovkonstantin364@gmail.com>
2020-11-19 10:10:35 +0100

    Update index.tsx

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import App from "./components/app";
+import App from "./components/App";
 import "bootstrap/dist/css/bootstrap.min.css";
 import ***REMOVED*** Provider***REMOVED*** from "react-redux";
 import store from "./store";

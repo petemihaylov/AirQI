9a7639b60a6387f38d5b4a8f6cca091594b30998
Mihaylov,Petar P <p.mihaylov@student.fontys.nl>
2020-11-19 09:25:09 +0100

    CRUD users and userActions

--- client/src/components/_pages/admin/adminboard.tsx
+++ client/src/components/_pages/admin/adminboard.tsx
@@ -5,15 +5,21 @@ import User from "../../../entities/User";
 import UserService from "../../../services/user.service";
 import ***REMOVED*** faCog, faTimes***REMOVED*** from "@fortawesome/free-solid-svg-icons";
 import "./css/style.css";
+import ***REMOVED*** connect***REMOVED*** from "react-redux";
+import ***REMOVED*** deleteUser, fetchUsers***REMOVED*** from "../../../actions/userActions";
 
 const AdminBoard = (props: any) => ***REMOVED***
   const [content, handleContent] = useState([]);
 
   useEffect(() => ***REMOVED***
-    UserService.getPublicContent().then((response) => ***REMOVED***
-      handleContent(response.data);
-   ***REMOVED***);
- ***REMOVED***, []);
+    props.dispatch(fetchUsers());
+    if (props.items !== []) handleContent(props.items);
+ ***REMOVED***, [props.items]);
+
+  const handleDelete = (id: any, index: number) => ***REMOVED***
+    console.log(id + ", " + index);
+    props.dispatch(deleteUser(id, index));
+ ***REMOVED***;
 
   return (
     <Container fluid>
@@ -30,32 +36,34 @@ const AdminBoard = (props: any) => ***REMOVED***
         </thead>
         <tbody>
           ***REMOVED***content !== [] &&
-            content.map((item: User) => (
+            content.map((item: User, index) => (
               <tr key=***REMOVED***item.id***REMOVED***>
                 <td>***REMOVED***item.id***REMOVED***</td>
-                <td> ***REMOVED***item.firstName***REMOVED*** ***REMOVED***item.lastName***REMOVED***</td>
+                <td>
+                  ***REMOVED***" "***REMOVED***
+                  ***REMOVED***item.firstName***REMOVED*** ***REMOVED***item.lastName***REMOVED***
+                </td>
                 <td>04/10/2013</td>
                 <td>***REMOVED***item.userRole***REMOVED***</td>
                 <td>
                   <span className="status text-success">&bull;</span> Active
                 </td>
                 <td>
-                  <a
-                    href="#"
+                  <button
                     className="settings"
                     title="Settings"
                     data-toggle="tooltip"
                   >
                     <FontAwesomeIcon icon=***REMOVED***faCog***REMOVED*** />
-                  </a>
-                  <a
-                    href="#"
+                  </button>
+                  <button
                     className="delete"
                     title="Delete"
                     data-toggle="tooltip"
+                    onClick=***REMOVED***handleDelete.bind(this, item.id, index)***REMOVED***
                   >
                     <FontAwesomeIcon icon=***REMOVED***faTimes***REMOVED*** />
-                  </a>
+                  </button>
                 </td>
               </tr>
             ))***REMOVED***
@@ -65,4 +73,11 @@ const AdminBoard = (props: any) => ***REMOVED***
   );
 ***REMOVED***;
 
-export default AdminBoard;
+function mapStateToProps(state: any) ***REMOVED***
+  const ***REMOVED*** items***REMOVED*** = state.users;
+  return ***REMOVED***
+    items,
+ ***REMOVED***;
+***REMOVED***
+
+export default connect(mapStateToProps)(AdminBoard);

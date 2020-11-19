3c89b12f7b2a3d62225b619f0f439357340d312b
Konstantin Mihaylov <mihailovkonstantin364@gmail.com>
2020-11-19 11:25:49 +0100

    Create Worker Service

---
+++ WorkerService/Program.cs
@@ -0,0 +1,24 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+namespace WorkerService
+***REMOVED***
+    public class Program
+    ***REMOVED***
+        public static void Main(string[] args)
+        ***REMOVED***
+            CreateHostBuilder(args).Build().Run();
+       ***REMOVED***
+
+        public static IHostBuilder CreateHostBuilder(string[] args) =>
+            Host.CreateDefaultBuilder(args)
+                .ConfigureServices((hostContext, services) =>
+                ***REMOVED***
+                    services.AddHostedService<Worker>();
+               ***REMOVED***);
+   ***REMOVED***
+***REMOVED***

d952b2ad851ae57243b642001e87d63d8d7073c8
Konstantin Mihaylov <mihailovkonstantin364@gmail.com>
2020-11-26 12:15:15 +0100

    [Hangfire] AirThings

---
+++ AirQi/Services/AirThings.cs
@@ -0,0 +1,18 @@
+using System;
+using System.Threading.Tasks;
+
+namespace AirQi
+***REMOVED***
+    public class AirThings : IRecurringJob
+    ***REMOVED***
+        public void ExecuteWorker()
+        ***REMOVED***
+            throw new NotImplementedException();
+       ***REMOVED***
+
+        public Task ExecuteWorkerAsync()
+        ***REMOVED***
+            throw new NotImplementedException();
+       ***REMOVED***
+   ***REMOVED***
+***REMOVED***

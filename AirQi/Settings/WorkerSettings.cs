69b1ea51749e692472a338a05a4100161e6f2690
Konstantin Mihaylov <mihailovkonstantin364@gmail.com>
2020-11-30 23:38:43 +0100

    AirThings

---
+++ AirQi/Settings/WorkerSettings.cs
@@ -0,0 +1,7 @@
+namespace AirQi.Settings
+***REMOVED***
+    public class WorkerSettings : IWorkerSettings
+    ***REMOVED***
+        public string OcpApimSubscriptionKey ***REMOVED*** get; set;***REMOVED***
+   ***REMOVED***
+***REMOVED***

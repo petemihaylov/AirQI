d957c75c78ffee035e6d3ce4723547efb2d532fb
Konstantin Mihaylov <mihailovkonstantin364@gmail.com>
2020-12-04 22:05:43 +0100

    Generate Tests

---
+++ AirQi.Test/UnitTest1.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace AirQi.Test
+***REMOVED***
+    [TestClass]
+    public class UnitTest1
+    ***REMOVED***
+        [TestMethod]
+        public void TestMethod1()
+        ***REMOVED***
+       ***REMOVED***
+   ***REMOVED***
+***REMOVED***

7a746c10990e7eca65002761c09575e88932689e
Mihaylov,Petar P <p.mihaylov@student.fontys.nl>
2020-12-06 01:25:24 +0100

    Adds IMarkerService initialization

---
+++ ApiBase/Services/Interfaces/IMarkerService.cs
@@ -0,0 +1,13 @@
+
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using ApiBase.DTOs;
+using ApiBase.Models;
+
+namespace ApiBase.Services.Interfaces
+{
+    public interface IMarkerService
+    {
+
+    }
+}

b8ef499c02e9bd42fa1f3713c6836954f8b60312
Mihaylov,Petar P <p.mihaylov@student.fontys.nl>
2020-12-07 08:55:02 +0100

    Adds Integration tests configuration

---
+++ ApiBase.IntegrationTests/UserControllerTest.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Text;
+using FluentAssertions;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace ApiBase.IntegrationTests
+***REMOVED***
+    public class UserControllerTest : IntegrationTest
+    ***REMOVED***
+        [Fact]
+        public async Task GetAllUsers_WithoutAnyUsers_ReturnsEmptyResponse()
+        ***REMOVED***
+            // Arrange
+
+            await AuthenticateAsync();
+
+            // Act
+            var response = await  TestClient.GetAsync("/api/users");
+
+            
+            // Assert
+            response.StatusCode.Should().Be(HttpStatusCode.OK);
+       ***REMOVED***
+   ***REMOVED***
+***REMOVED***

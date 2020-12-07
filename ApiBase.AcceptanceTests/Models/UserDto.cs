3bed4ea6634f7c6bb99516545145b29893dc2b42
Mihaylov,Petar P <p.mihaylov@student.fontys.nl>
2020-12-08 00:06:00 +0100

    Adds End-to-End test Acceptance test

---
+++ ApiBase.AcceptanceTests/Models/UserDto.cs
@@ -0,0 +1,12 @@
+﻿namespace ApiBase.AcceptanceTests.Models
+***REMOVED***
+    public class UserDto
+    ***REMOVED***
+        public string Username ***REMOVED*** get; set;***REMOVED***
+        public string Password ***REMOVED*** get; set;***REMOVED***
+        public string FirstName ***REMOVED*** get; set;***REMOVED***
+        public string LastName ***REMOVED*** get; set;***REMOVED***
+        public string UserRole ***REMOVED*** get; set;***REMOVED***
+   ***REMOVED***
+
+***REMOVED***

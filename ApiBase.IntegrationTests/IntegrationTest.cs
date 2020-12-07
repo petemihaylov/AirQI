af07609d49c15eb7d416beb4810a9ca83432d71e
Mihaylov,Petar P <p.mihaylov@student.fontys.nl>
2020-12-07 20:17:40 +0100

    Adds external test db and makes migration

--- ApiBase.IntegrationTests/IntegrationTest.cs
+++ ApiBase.IntegrationTests/IntegrationTest.cs
@@ -1,4 +1,6 @@
-﻿using System.Net.Http;
+﻿using System;
+using System.Linq;
+using System.Net.Http;
 using System.Net.Http.Headers;
 using System.Threading.Tasks;
 using ApiBase.Data;
@@ -7,6 +9,7 @@ using Microsoft.AspNetCore.Mvc.Testing;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.DependencyInjection.Extensions;
+using Microsoft.Extensions.Logging;
 
 namespace ApiBase.IntegrationTests
 {
@@ -15,16 +18,30 @@ namespace ApiBase.IntegrationTests
         protected readonly HttpClient TestClient;
         protected IntegrationTest()
         {
+
             var appFactory = new WebApplicationFactory<Startup>()
                 .WithWebHostBuilder(builder =>
                 {
                     builder.ConfigureServices(services =>
                     {
-                        services.RemoveAll(typeof(ApplicationContext));
+                        var descriptor = services.SingleOrDefault(
+                            d => d.ServiceType ==
+                                 typeof(DbContextOptions<ApplicationContext>));
+
+                        services.Remove(descriptor);
                         services.AddDbContext<ApplicationContext>((options, context) =>
                         {
-                            context.UseSqlServer("");
+                            context.UseSqlServer("Server=tcp:airqi.database.windows.net,1433;Initial Catalog=testairdb;Persist Security Info=False;User ID=airqi;Password=Qazwsx098;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;");
                         });
+
+                        var sp = services.BuildServiceProvider();
+                        using (var scope = sp.CreateScope())
+                        {
+                            var scopedServices = scope.ServiceProvider;
+                            var db = scopedServices.GetRequiredService<ApplicationContext>();
+                            db.Database.EnsureCreated();
+                        }
+
                     });
                 });
             TestClient = appFactory.CreateClient();
--- ApiBase.IntegrationTests/UserControllerTest.cs
+++ ApiBase.IntegrationTests/UserControllerTest.cs
@@ -1,9 +1,9 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Net;
-using System.Text;
+using System.Net.Http;
 using FluentAssertions;
 using System.Threading.Tasks;
+using ApiBase.Models;
 using Xunit;
 
 namespace ApiBase.IntegrationTests
@@ -13,9 +13,11 @@ namespace ApiBase.IntegrationTests
         [Fact]
         public async Task GetAllUsers_WithoutAnyUsers_ReturnsEmptyResponse()
         {
-            // Arrange
 
+            // Arrange
             await AuthenticateAsync();
+            var list = new List<User>() { new User() { Id = 1, FirstName = "Test", LastName = "User", Password = "test", UserRole = "User", Username = "test" } };
+
 
             // Act
             var response = await  TestClient.GetAsync("/api/users");
@@ -23,6 +25,7 @@ namespace ApiBase.IntegrationTests
             
             // Assert
             response.StatusCode.Should().Be(HttpStatusCode.OK);
+            (await response.Content.ReadAsAsync<List<User>>()).ShouldAllBeEquivalentTo(list);
         }
     }
 }

4f4a364b18491d2395006819bbcae0f9444873e3
Konstantin Mihaylov <mihailovkonstantin364@gmail.com>
2020-12-13 02:21:04 +0100

    Update signalR

--- Web/airqi/src/components/Dashboard.js
+++ Web/airqi/src/components/Dashboard.js
@@ -1,19 +1,46 @@
+import ***REMOVED*** HubConnectionBuilder***REMOVED*** from "@microsoft/signalr";
 import React, ***REMOVED*** useEffect, useState***REMOVED*** from 'react';
 import fetchStationsData from '../services/pull_stations';
 
+const ***REMOVED*** REACT_APP_API_URL***REMOVED*** = process.env;
 export default function Dashboard() ***REMOVED***
+
+    const [connection, setConnection] = useState(null);
     const [stations, setStationData] = useState([]);
+
+    useEffect(() => ***REMOVED***        
+        const newConnection = new HubConnectionBuilder()
+            .withUrl(REACT_APP_API_URL + "livestations")
+            .withAutomaticReconnect()
+            .build();
+      
+        setConnection(newConnection);
+   ***REMOVED***, []);
 
     useEffect(() => ***REMOVED***
         fetchStationsData()
-            .then(json => ***REMOVED***
-                setStationData(json.data);
+        .then(json => ***REMOVED***
+            setStationData(json.data);
+       ***REMOVED***)
+        .catch(err => ***REMOVED***
+            console.error(err.message);
+       ***REMOVED***);
+   ***REMOVED***, []);
+
+    useEffect(() => ***REMOVED***
+        if (connection) ***REMOVED***
+            connection
+            .start()
+            .then((result) => ***REMOVED***
+                console.log("Connected!");
+
+                connection.on("GetNewStations", (station) => ***REMOVED***
+                setStationData([station]);
+               ***REMOVED***);
            ***REMOVED***)
-            .catch(err => ***REMOVED***
-                console.error(err.message);
-           ***REMOVED***);
-        
-   ***REMOVED***);
+            .catch((e) => console.log("Connection failed: ", e));
+       ***REMOVED***
+   ***REMOVED***, [connection]);
 
     return (
         <div>

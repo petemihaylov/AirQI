3686ccdb0645782cce5db7333a6e6385b86e4503
Konstantin Mihaylov <mihailovkonstantin364@gmail.com>
2020-12-13 02:21:04 +0100

    Update signalR

--- Web/airqi/src/components/Dashboard.js
+++ Web/airqi/src/components/Dashboard.js
@@ -1,19 +1,46 @@
+import { HubConnectionBuilder } from "@microsoft/signalr";
 import React, { useEffect, useState } from 'react';
 import fetchStationsData from '../services/pull_stations';
 
+const { REACT_APP_API_URL } = process.env;
 export default function Dashboard() {
+
+    const [connection, setConnection] = useState(null);
     const [stations, setStationData] = useState([]);
+
+    useEffect(() => {        
+        const newConnection = new HubConnectionBuilder()
+            .withUrl(REACT_APP_API_URL + "livestations")
+            .withAutomaticReconnect()
+            .build();
+      
+        setConnection(newConnection);
+    }, []);
 
     useEffect(() => {
         fetchStationsData()
-            .then(json => {
-                setStationData(json.data);
+        .then(json => {
+            setStationData(json.data);
+        })
+        .catch(err => {
+            console.error(err.message);
+        });
+    }, []);
+
+    useEffect(() => {
+        if (connection) {
+            connection
+            .start()
+            .then((result) => {
+                console.log("Connected!");
+
+                connection.on("GetNewStations", (station) => {
+                setStationData([station]);
+                });
             })
-            .catch(err => {
-                console.error(err.message);
-            });
-        
-    });
+            .catch((e) => console.log("Connection failed: ", e));
+        }
+    }, [connection]);
 
     return (
         <div>

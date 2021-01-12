bf4f494237bd4da0ced60f384e3644d3909179d8
Konstantin Mihaylov <mihailovkonstantin364@gmail.com>
2021-01-12 17:12:09 +0100

    xUnit Tests

---
+++ Data/Qi.Tests/StationsControllerTests.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace Qi.Tests
+***REMOVED***
+    public class StationsControllerTests
+    ***REMOVED***
+        [Fact]
+        public void Test1()
+        ***REMOVED***
+
+       ***REMOVED***
+   ***REMOVED***
+***REMOVED***

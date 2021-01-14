a5f1698c3ca4965c01f5d9982ec587605c5704a9
Konstantin Mihaylov <mihailovkonstantin364@gmail.com>
2021-01-14 20:18:47 +0100

    Stations xUnit

--- Data/AirQi/Tests/StationsControllerTests.cs
+++ Data/AirQi/Tests/StationsControllerTests.cs
@@ -1,15 +1,34 @@
-using System;
+using AirQi.Controllers;
+using AirQi.Models.Core;
+using AirQi.Repository;
+using AutoMapper;
+using Moq;
 using Xunit;
 
 namespace Qi.Tests
 {
     public class StationsControllerTests
     {
-        [Fact]
-        public void Test()
+        private readonly Mock<IMongoDataRepository<Station>> _repository;
+        private readonly Mock<IMapper> _mapper;
+
+        private readonly StationsController _controller;
+
+        public StationsControllerTests()
         {
+            this._repository = new Mock<IMongoDataRepository<Station>>();
+            this._mapper = new Mock<IMapper>();
+            this._controller = new StationsController(Repository.Object, Mapper.Object);
+        }
 
+        public Mock<IMongoDataRepository<Station>> Repository => _repository;
 
+        public Mock<IMapper> Mapper => _mapper;
+
+        [Fact]
+        public void Test_GetAllStations_NotFound()
+        {
+            
         }
     }
 }

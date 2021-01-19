274d060833a2865792716283848ff1e87e72a9f0
Konstantin Mihaylov <mihailovkonstantin364@gmail.com>
2021-01-20 00:24:14 +0100

    Live Map

--- Data/AirQi/Program.cs
+++ Data/AirQi/Program.cs
@@ -15,6 +15,7 @@ namespace AirQi
                 .ConfigureWebHostDefaults(webBuilder =>
                 ***REMOVED***
                     webBuilder.UseStartup<Startup>();
+                    webBuilder.UseUrls("http://localhost:9000/");
                ***REMOVED***);
    ***REMOVED***
 ***REMOVED***

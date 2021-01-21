d454b70b1f24a44a7d47f7a982319efcfabb006e
Konstantin Mihaylov <mihailovkonstantin364@gmail.com>
2021-01-21 11:36:20 +0100

    Create Measurements xUnit Tests

--- Data/AirQi/Tests/MeasurementsControllerTests.cs
+++ Data/AirQi/Tests/MeasurementsControllerTests.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections.Generic;
 using System.Threading.Tasks;
 using AirQi.Controllers;
@@ -10,6 +11,7 @@ using AssetNXT.Hubs;
 using AutoMapper;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.SignalR;
+using MongoDB.Bson;
 using Moq;
 using Xunit;
 
@@ -72,6 +74,119 @@ namespace Qi.Tests
             Assert.IsType<NotFoundResult>(notFoundResult.Result);
        ***REMOVED***
 
+        [Fact]
+        public void Test_GetMeasurementByStationId_ReturnsRightItem()
+        ***REMOVED***
+            // Arrange
+            var mockRepo = new Mock<IMongoDataRepository<Station>>();
+
+            double[] position = new double[] ***REMOVED*** 30.2, 50.3***REMOVED***;
+            Station station = this.Mock.MockStation(position);
+            station.Id = ObjectId.GenerateNewId();
+            station.CreatedAt = station.UpdatedAt = DateTime.UtcNow;
+
+            mockRepo.Setup(repo => repo.GetObjectByIdAsync(station.Id.ToString()))
+            .Returns(Task.FromResult(station));
+
+            var controller = new MeasurementsController(mockRepo.Object, Mapper);
+            
+            // Act       
+            var okResult = controller.GetMeasurementsByStationId(station.Id.ToString()).Result as OkObjectResult;
+
+            // Assert
+            Assert.IsType<StationMeasurementReadDto>(okResult.Value);
+            Assert.Equal(station.Id.ToString(), (okResult.Value as StationMeasurementReadDto).Id);           
+       ***REMOVED***
+
+        [Fact]
+        public void Test_GetMeasurementsByStationId_ReturnsNotFoundResult()
+        ***REMOVED***
+            // Arrange
+            var mockRepo = new Mock<IMongoDataRepository<Station>>();
+            var controller = new MeasurementsController(mockRepo.Object, Mapper);
+            this.Mock.GenerateMockStations();
+
+            // Act
+            var notFoundResult = controller.GetMeasurementsByStationId(ObjectId.GenerateNewId().ToString());
+        
+            // Assert
+            Assert.IsType<NotFoundResult>(notFoundResult.Result);
+       ***REMOVED***
+
+        [Fact]
+        public void Test_UpdateMeasurements_ReturnsOkResult()
+        ***REMOVED***
+            // Arrange
+            var mockRepo = new Mock<IMongoDataRepository<Station>>();
+
+            double[] position = new double[] ***REMOVED*** 30.2, 50.3***REMOVED***;
+            Station station = this.Mock.MockStation(position);
+            station.Id = ObjectId.GenerateNewId();
+            station.CreatedAt = station.UpdatedAt = DateTime.UtcNow;
+
+            mockRepo.Setup(repo => repo.GetObjectByIdAsync(station.Id.ToString()))
+            .Returns(Task.FromResult(station));
+
+            var controller = new MeasurementsController(mockRepo.Object, Mapper);
+            
+            // Act       
+            var okResult = controller.UpdateMeasurements(station.Id.ToString(), this.Mapper.Map<StationMeasurementCreateDto>(station));
+
+            // Assert
+            Assert.IsType<OkObjectResult>(okResult.Result); 
+            Assert.IsType<StationReadDto>((okResult.Result as OkObjectResult).Value);           
+       ***REMOVED***
+
+        [Fact]
+        public void Test_UpdateMeasurements_ReturnsNotFoundResult()
+        ***REMOVED***
+            // Arrange
+            var mockRepo = new Mock<IMongoDataRepository<Station>>();
+            var controller = new MeasurementsController(mockRepo.Object, Mapper);
+
+            // Act
+            var notFoundResult = controller.UpdateMeasurements(ObjectId.GenerateNewId().ToString(), (StationMeasurementCreateDto) null);
+        
+            // Assert
+            Assert.IsType<NotFoundResult>(notFoundResult.Result);
+       ***REMOVED***
+
+        [Fact]
+        public void Test_DeleteMeasurement_ReturnsOkResult()
+        ***REMOVED***
+            // Arrange
+            var mockRepo = new Mock<IMongoDataRepository<Station>>();
+
+            double[] position = new double[] ***REMOVED*** 30.2, 50.3***REMOVED***;
+            Station station = this.Mock.MockStation(position);
+            station.Id = ObjectId.GenerateNewId();
+            station.CreatedAt = station.UpdatedAt = DateTime.UtcNow;
+
+            mockRepo.Setup(repo => repo.GetObjectByIdAsync(station.Id.ToString()))
+            .Returns(Task.FromResult(station));
+
+            var controller = new MeasurementsController(mockRepo.Object, Mapper);
+            
+            // Act       
+            var okResult = controller.DeleteStation(station.Id.ToString());
+
+            // Assert
+            Assert.IsType<OkObjectResult>(okResult.Result);           
+       ***REMOVED***
+
+        [Fact]
+        public void Test_DeleteStation_ReturnsNotFoundResult()
+        ***REMOVED***
+            // Arrange
+            var mockRepo = new Mock<IMongoDataRepository<Station>>();
+            var controller = new MeasurementsController(mockRepo.Object, Mapper);
+
+            // Act
+            var notFoundResult = controller.DeleteStation(ObjectId.GenerateNewId().ToString());
+        
+            // Assert
+            Assert.IsType<NotFoundResult>(notFoundResult.Result);
+       ***REMOVED***
         
    ***REMOVED***
 ***REMOVED***

f6785f73ea57041a9e076039ab67689faaef644d
Konstantin Mihaylov <mihailovkonstantin364@gmail.com>
2021-01-21 23:40:48 +0100

    Agreement Service

---
+++ Data/AirQi/Dtos/Data/ServiceAgreementCreateDto.cs
@@ -0,0 +1,13 @@
+using System;
+using System.Collections.Generic;
+using AirQi.Models.Core;
+
+namespace AirQi.Dtos.Data
+***REMOVED***
+    public class ServiceAgreementCreateDto
+    ***REMOVED***
+
+        public List<Station> Stations ***REMOVED*** get; set;***REMOVED***
+
+   ***REMOVED***
+***REMOVED***

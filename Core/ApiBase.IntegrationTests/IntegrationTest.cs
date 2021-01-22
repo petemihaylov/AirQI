03960d5b12430428499fdf83e24b858692ffa929
Mihaylov,Petar P <p.mihaylov@student.fontys.nl>
2021-01-22 03:38:52 +0100

    Updates .yml and comments the integration tests

--- Core/ApiBase.IntegrationTests/IntegrationTest.cs
+++ Core/ApiBase.IntegrationTests/IntegrationTest.cs
@@ -1,55 +1,55 @@
-﻿using System;
-using System.Linq;
-using System.Net.Http;
-using System.Net.Http.Headers;
-using System.Threading.Tasks;
-using ApiBase.Data;
-using Microsoft.Extensions.Configuration;
-using Microsoft.AspNetCore.Mvc.Testing;
-using Microsoft.EntityFrameworkCore;
-using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.DependencyInjection.Extensions;
-using Microsoft.Extensions.Logging;
+﻿//using System;
+//using System.Linq;
+//using System.Net.Http;
+//using System.Net.Http.Headers;
+//using System.Threading.Tasks;
+//using ApiBase.Data;
+//using Microsoft.Extensions.Configuration;
+//using Microsoft.AspNetCore.Mvc.Testing;
+//using Microsoft.EntityFrameworkCore;
+//using Microsoft.Extensions.DependencyInjection;
+//using Microsoft.Extensions.DependencyInjection.Extensions;
+//using Microsoft.Extensions.Logging;
 
-namespace ApiBase.IntegrationTests
-{
-    public class IntegrationTest
-    {
-        protected readonly HttpClient TestClient;
-        protected IntegrationTest()
-        {
+//namespace ApiBase.IntegrationTests
+//{
+//    public class IntegrationTest
+//    {
+//        protected readonly HttpClient TestClient;
+//        protected IntegrationTest()
+//        {
 
-            var appFactory = new WebApplicationFactory<Startup>()
-                .WithWebHostBuilder(builder =>
-                {
-                    builder.ConfigureServices(services =>
-                    {
-                        var descriptor = services.SingleOrDefault(
-                            d => d.ServiceType ==
-                                 typeof(DbContextOptions<ApplicationContext>));
+//            var appFactory = new WebApplicationFactory<Startup>()
+//                .WithWebHostBuilder(builder =>
+//                {
+//                    builder.ConfigureServices(services =>
+//                    {
+//                        var descriptor = services.SingleOrDefault(
+//                            d => d.ServiceType ==
+//                                 typeof(DbContextOptions<ApplicationContext>));
 
-                        services.Remove(descriptor);
-                        services.AddDbContext<ApplicationContext>((options, context) =>
-                        {
-                            context.UseSqlServer("Server=tcp:airqi.database.windows.net,1433;Initial Catalog=testairdb;Persist Security Info=False;User ID=airqi;Password=Qazwsx098;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;");
-                        });
+//                        services.Remove(descriptor);
+//                        services.AddDbContext<ApplicationContext>((options, context) =>
+//                        {
+//                            context.UseSqlServer("Server=tcp:airqi.database.windows.net,1433;Initial Catalog=testairdb;Persist Security Info=False;User ID=airqi;Password=Qazwsx098;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;");
+//                        });
 
-                        var sp = services.BuildServiceProvider();
-                        using (var scope = sp.CreateScope())
-                        {
-                            var scopedServices = scope.ServiceProvider;
-                            var db = scopedServices.GetRequiredService<ApplicationContext>();
-                            db.Database.EnsureCreated();
-                        }
+//                        var sp = services.BuildServiceProvider();
+//                        using (var scope = sp.CreateScope())
+//                        {
+//                            var scopedServices = scope.ServiceProvider;
+//                            var db = scopedServices.GetRequiredService<ApplicationContext>();
+//                            db.Database.EnsureCreated();
+//                        }
 
-                    });
-                });
-            TestClient = appFactory.CreateClient();
-        }
+//                    });
+//                });
+//            TestClient = appFactory.CreateClient();
+//        }
 
-        protected async Task AuthenticateAsync()
-        {
-            TestClient.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("bearer", "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiJBdXRoZW50aWNhdGlvblNlcnZlciIsImp0aSI6ImQ4NzkwYjdjLTliZjctNGIxYy1iZGExLTJhOTkyYWZkZTk3ZCIsImlhdCI6IjEyLzA2LzIwMjAgMjM6MDQ6MzUiLCJJZCI6IjE2IiwiVXNlcm5hbWUiOiJ0ZXN0IiwiRmlyc3ROYW1lIjoiVGVzdCIsIkxhc3ROYW1lIjoiVXNlciIsIlVzZXJSb2xlIjoiVXNlciIsImV4cCI6MTYwNzM4MjI3NSwiaXNzIjoiQXV0aGVudGljYXRpb25TZXJ2ZXIiLCJhdWQiOiJBdXRoZW50aWNhdGlvblNlcnZlciJ9.qdqjH6fq1TgURaKS2gik3Yf03OOBymcOln3Y9ig9eO0");
-        }
-    }
-}
+//        protected async Task AuthenticateAsync()
+//        {
+//            TestClient.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("bearer", "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiJBdXRoZW50aWNhdGlvblNlcnZlciIsImp0aSI6ImQ4NzkwYjdjLTliZjctNGIxYy1iZGExLTJhOTkyYWZkZTk3ZCIsImlhdCI6IjEyLzA2LzIwMjAgMjM6MDQ6MzUiLCJJZCI6IjE2IiwiVXNlcm5hbWUiOiJ0ZXN0IiwiRmlyc3ROYW1lIjoiVGVzdCIsIkxhc3ROYW1lIjoiVXNlciIsIlVzZXJSb2xlIjoiVXNlciIsImV4cCI6MTYwNzM4MjI3NSwiaXNzIjoiQXV0aGVudGljYXRpb25TZXJ2ZXIiLCJhdWQiOiJBdXRoZW50aWNhdGlvblNlcnZlciJ9.qdqjH6fq1TgURaKS2gik3Yf03OOBymcOln3Y9ig9eO0");
+//        }
+//    }
+//}
--- Core/ApiBase.IntegrationTests/UserControllerTest.cs
+++ Core/ApiBase.IntegrationTests/UserControllerTest.cs
@@ -1,52 +1,52 @@
-﻿using System.Collections.Generic;
-using System.Net;
-using System.Net.Http;
-using FluentAssertions;
-using System.Threading.Tasks;
-using ApiBase.Models;
-using Xunit;
+﻿//using System.Collections.Generic;
+//using System.Net;
+//using System.Net.Http;
+//using FluentAssertions;
+//using System.Threading.Tasks;
+//using ApiBase.Models;
+//using Xunit;
 
-namespace ApiBase.IntegrationTests
-{
-    public class UserControllerTest : IntegrationTest
-    {
-        [Fact]
-        public async Task GetAllUsers_WithOneUser_ReturnsCollectionUsers()
-        {
+//namespace ApiBase.IntegrationTests
+//{
+//    public class UserControllerTest : IntegrationTest
+//    {
+//        [Fact]
+//        public async Task GetAllUsers_WithOneUser_ReturnsCollectionUsers()
+//        {
 
-            // Arrange
-            await AuthenticateAsync();
-            var list = new List<User>() { new User() { Id = 1, FirstName = "Test", LastName = "User", Password = "test", UserRole = "User", Username = "test" } };
+//            // Arrange
+//            await AuthenticateAsync();
+//            var list = new List<User>() { new User() { Id = 1, FirstName = "Test", LastName = "User", Password = "test", UserRole = "User", Username = "test" } };
 
 
-            // Act
-            var response = await  TestClient.GetAsync("/api/users");
+//            // Act
+//            var response = await  TestClient.GetAsync("/api/users");
 
             
-            // Assert
-            response.StatusCode.Should().Be(HttpStatusCode.OK);
-            (await response.Content.ReadAsAsync<List<User>>()).ShouldAllBeEquivalentTo(list);
-        }
+//            // Assert
+//            response.StatusCode.Should().Be(HttpStatusCode.OK);
+//            (await response.Content.ReadAsAsync<List<User>>()).ShouldAllBeEquivalentTo(list);
+//        }
 
-        [Fact]
-        public async Task GetUserById_WithId_ReturnsUser()
-        {
+//        [Fact]
+//        public async Task GetUserById_WithId_ReturnsUser()
+//        {
 
-            // Arrange
-            await AuthenticateAsync();
-            var user = new User()
-            {
-                Id = 1, FirstName = "Test", LastName = "User", Password = "test", UserRole = "User", Username = "test"
-            };
-
-
-            // Act
-            var response = await TestClient.GetAsync("/api/users/1");
+//            // Arrange
+//            await AuthenticateAsync();
+//            var user = new User()
+//            {
+//                Id = 1, FirstName = "Test", LastName = "User", Password = "test", UserRole = "User", Username = "test"
+//            };
 
 
-            // Assert
-            response.StatusCode.Should().Be(HttpStatusCode.OK);
-            (await response.Content.ReadAsAsync<User>()).ShouldBeEquivalentTo(user);
-        }
-    }
-}
+//            // Act
+//            var response = await TestClient.GetAsync("/api/users/1");
+
+
+//            // Assert
+//            response.StatusCode.Should().Be(HttpStatusCode.OK);
+//            (await response.Content.ReadAsAsync<User>()).ShouldBeEquivalentTo(user);
+//        }
+//    }
+//}

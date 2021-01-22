2ec9fc1fcb21e7f4344fa554e306a926fa0f644c
Konstantin Mihaylov <mihailovkonstantin364@gmail.com>
2021-01-22 01:28:13 +0100

    Create Legend

---
+++ Web/client/src/assets/js/legend/legend.tsx
@@ -0,0 +1,32 @@
+import React, { useEffect } from "react";
+
+import * as legend from "./js/legend";
+import "./css/style.css";
+
+export const Legend = (props: any) => {
+  useEffect(() => {
+    legend.illustation();
+  }, []);
+
+  return (
+    <div className="mr-4 fixed-bottom">
+        <div className="row flex-row-reverse">
+          <div className="card align-self-center">
+            <div className="card-body">
+              <div id="legend">
+                <h5 className="card-title text-center">Health Risk</h5>
+                <div className="scale">
+                  <div id="gradient-bar"></div>
+                  <div className="indicator">Very High</div>
+                  <div className="indicator">High</div>
+                  <div className="indicator">Medium</div>
+                  <div className="indicator">Low</div>
+                  <div className="indicator">Very Low</div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+  );
+};
